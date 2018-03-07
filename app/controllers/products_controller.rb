897c6747c70f0bdf3fbf3de94d854fcc2cd78b1f
Imron Khatib <imronskhatib@gmail.com>
2018-03-06 19:50:07 -0600

    Create JSON for new_shoe

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,8 @@
 class ProductsController < ApplicationController
+
+  def nike_shoe
+    new_shoe = Product.all
+
+    render json: new_shoe.as_json
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,2 @@
 nike_shoes = Product.new(name: "Nike", price: 100, description: "This is the best type of shoe, there's just no  question about it.")
-
 nike_shoes.save
