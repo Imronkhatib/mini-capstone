4088e061cf241889d23e16b0147767f4d1326c04
Imron Khatib <imronskhatib@gmail.com>
2018-03-08 18:56:06 -0600

    create url segment

--- app/controllers/v1/products_controller.rb
+++ app/controllers/v1/products_controller.rb
@@ -11,7 +11,7 @@ class V1::ProductsController < ApplicationController
   end
   def any
     #get user input
-    user_input = params[:name]
+    user_input = params[:id]
     #based on the user input, find a product in the db
     the_product = Product.find_by(id: user_input)
     #show that product to the user
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
     get '/product-page' => 'products#all_products'
     get '/walets' => 'products#walet'
     get '/any-product' => 'products#any'
+    get '/the-product/:id' => 'products#any'
   end
 end
