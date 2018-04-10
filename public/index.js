c8c57cc44105f610e62b1c03f56d8b551e59e26e
Imron Khatib <imronskhatib@gmail.com>
2018-04-09 21:01:43 -0500

    Fix db error, add js server requeset axios

--- public/index.js
+++ public/index.js
@@ -1,4 +1,15 @@
 var productTemplate = document.querySelector('#product-template');
 var productContainer = document.querySelector('.row');
+ 
 
-productContainer.appendChild(productTemplate.content.cloneNode(true));
+/* global axios */
+
+axios.get("http://localhost:3000/v1/products").then(function(response) {
+  // console.log(response.data);
+  var products = response.data;
+
+  for (var i = 0; i < products.length; i++) {
+    var productContainer = productTemplate.content.cloneNode(true);
+      productContainer.querySelector('.card-title').innerText = products[i].name;
+  }
+});
